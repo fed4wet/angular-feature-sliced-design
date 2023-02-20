3d599b086752f275a73d693627034427f80cd6c2
oleg.Sypiahin <oleg.Sypiahin@flolive.net>
2023-02-20 11:41:33 +0200

    master: update versions and added decorate-angular-cli.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'fs-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {}
